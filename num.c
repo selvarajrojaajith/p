21cb66863cc8426d72ce81773c23fe8b757c0d38
selvarajrojaajith <36071813+selvarajrojaajith@users.noreply.github.com>
2018-02-12 16:34:41 +0530

    abc.c
    good

---
+++ num.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+int main()
+{
+    int count = 0;
+    printf("Enter an integer: ");
+    scanf("%lld", &n);
+    while(n != 0)
+    {
+       n\=10;
+       ++count;
+    }
+    printf("Number of digits: %d", count);
+}
